7806780719e1a96f1a81994c6a3bebffc37fc4a5
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-12-22 10:28:41 +0700

    add editor choice models

---
+++ modules/digital/models/DigitalChoice.php
@@ -0,0 +1,313 @@
+<?php
+/**
+ * DigitalChoice
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 22 December 2016, 10:28 WIB
+ * @link http://company.ommu.co
+ * @contact (+62)856-299-4114
+ *
+ * This is the template for generating the model class of a specified table.
+ * - $this: the ModelCode object
+ * - $tableName: the table name for this class (prefix is already removed if necessary)
+ * - $modelClass: the model class name
+ * - $columns: list of table columns (name=>CDbColumnSchema)
+ * - $labels: list of attribute labels (name=>label)
+ * - $rules: list of validation rules
+ * - $relations: list of relations (name=>relation declaration)
+ *
+ * --------------------------------------------------------------------------------------
+ *
+ * This is the model class for table "ommu_digital_choice".
+ *
+ * The followings are the available columns in table 'ommu_digital_choice':
+ * @property string $choice_id
+ * @property string $digital_id
+ * @property string $user_id
+ * @property string $choice_date
+ * @property string $choice_ip
+ *
+ * The followings are the available model relations:
+ * @property OmmuDigitals $digital
+ */
+class DigitalChoice extends CActiveRecord
+{
+	public $defaultColumns = array();
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return DigitalChoice the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'ommu_digital_choice';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('digital_id, user_id, choice_date, choice_ip', 'required'),
+			array('digital_id, user_id', 'length', 'max'=>11),
+			array('choice_ip', 'length', 'max'=>20),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('choice_id, digital_id, user_id, choice_date, choice_ip', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'digital_relation' => array(self::BELONGS_TO, 'OmmuDigitals', 'digital_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'choice_id' => Yii::t('attribute', 'Choice'),
+			'digital_id' => Yii::t('attribute', 'Digital'),
+			'user_id' => Yii::t('attribute', 'User'),
+			'choice_date' => Yii::t('attribute', 'Choice Date'),
+			'choice_ip' => Yii::t('attribute', 'Choice Ip'),
+		);
+		/*
+			'Choice' => 'Choice',
+			'Digital' => 'Digital',
+			'User' => 'User',
+			'Choice Date' => 'Choice Date',
+			'Choice Ip' => 'Choice Ip',
+		
+		*/
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('t.choice_id',strtolower($this->choice_id),true);
+		if(isset($_GET['digital']))
+			$criteria->compare('t.digital_id',$_GET['digital']);
+		else
+			$criteria->compare('t.digital_id',$this->digital_id);
+		if(isset($_GET['user']))
+			$criteria->compare('t.user_id',$_GET['user']);
+		else
+			$criteria->compare('t.user_id',$this->user_id);
+		if($this->choice_date != null && !in_array($this->choice_date, array('0000-00-00 00:00:00', '0000-00-00')))
+			$criteria->compare('date(t.choice_date)',date('Y-m-d', strtotime($this->choice_date)));
+		$criteria->compare('t.choice_ip',strtolower($this->choice_ip),true);
+
+		if(!isset($_GET['DigitalChoice_sort']))
+			$criteria->order = 't.choice_id DESC';
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+			'pagination'=>array(
+				'pageSize'=>30,
+			),
+		));
+	}
+
+
+	/**
+	 * Get column for CGrid View
+	 */
+	public function getGridColumn($columns=null) {
+		if($columns !== null) {
+			foreach($columns as $val) {
+				/*
+				if(trim($val) == 'enabled') {
+					$this->defaultColumns[] = array(
+						'name'  => 'enabled',
+						'value' => '$data->enabled == 1? "Ya": "Tidak"',
+					);
+				}
+				*/
+				$this->defaultColumns[] = $val;
+			}
+		} else {
+			//$this->defaultColumns[] = 'choice_id';
+			$this->defaultColumns[] = 'digital_id';
+			$this->defaultColumns[] = 'user_id';
+			$this->defaultColumns[] = 'choice_date';
+			$this->defaultColumns[] = 'choice_ip';
+		}
+
+		return $this->defaultColumns;
+	}
+
+	/**
+	 * Set default columns to display
+	 */
+	protected function afterConstruct() {
+		if(count($this->defaultColumns) == 0) {
+			/*
+			$this->defaultColumns[] = array(
+				'class' => 'CCheckBoxColumn',
+				'name' => 'id',
+				'selectableRows' => 2,
+				'checkBoxHtmlOptions' => array('name' => 'trash_id[]')
+			);
+			*/
+			$this->defaultColumns[] = array(
+				'header' => 'No',
+				'value' => '$this->grid->dataProvider->pagination->currentPage*$this->grid->dataProvider->pagination->pageSize + $row+1'
+			);
+			$this->defaultColumns[] = 'digital_id';
+			$this->defaultColumns[] = 'user_id';
+			$this->defaultColumns[] = array(
+				'name' => 'choice_date',
+				'value' => 'Utility::dateFormat($data->choice_date)',
+				'htmlOptions' => array(
+					'class' => 'center',
+				),
+				'filter' => Yii::app()->controller->widget('zii.widgets.jui.CJuiDatePicker', array(
+					'model'=>$this,
+					'attribute'=>'choice_date',
+					'language' => 'ja',
+					'i18nScriptFile' => 'jquery.ui.datepicker-en.js',
+					//'mode'=>'datetime',
+					'htmlOptions' => array(
+						'id' => 'choice_date_filter',
+					),
+					'options'=>array(
+						'showOn' => 'focus',
+						'dateFormat' => 'dd-mm-yy',
+						'showOtherMonths' => true,
+						'selectOtherMonths' => true,
+						'changeMonth' => true,
+						'changeYear' => true,
+						'showButtonPanel' => true,
+					),
+				), true),
+			);
+			$this->defaultColumns[] = 'choice_ip';
+		}
+		parent::afterConstruct();
+	}
+
+	/**
+	 * User get information
+	 */
+	public static function getInfo($id, $column=null)
+	{
+		if($column != null) {
+			$model = self::model()->findByPk($id,array(
+				'select' => $column
+			));
+			return $model->$column;
+			
+		} else {
+			$model = self::model()->findByPk($id);
+			return $model;			
+		}
+	}
+
+	/**
+	 * before validate attributes
+	 */
+	/*
+	protected function beforeValidate() {
+		if(parent::beforeValidate()) {
+			// Create action
+		}
+		return true;
+	}
+	*/
+
+	/**
+	 * after validate attributes
+	 */
+	/*
+	protected function afterValidate()
+	{
+		parent::afterValidate();
+			// Create action
+		return true;
+	}
+	*/
+	
+	/**
+	 * before save attributes
+	 */
+	/*
+	protected function beforeSave() {
+		if(parent::beforeSave()) {
+		}
+		return true;	
+	}
+	*/
+	
+	/**
+	 * After save attributes
+	 */
+	/*
+	protected function afterSave() {
+		parent::afterSave();
+		// Create action
+	}
+	*/
+
+	/**
+	 * Before delete attributes
+	 */
+	/*
+	protected function beforeDelete() {
+		if(parent::beforeDelete()) {
+			// Create action
+		}
+		return true;
+	}
+	*/
+
+	/**
+	 * After delete attributes
+	 */
+	/*
+	protected function afterDelete() {
+		parent::afterDelete();
+		// Create action
+	}
+	*/
+
+}
